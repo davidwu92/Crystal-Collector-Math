01b83a95c8f9956fa2deaf1096ddabb59c89eebe
David Wu <davidjwu92@gmail.com>
2019-09-02 16:59:05 -0700

    fixed 0 bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,10 @@ let wins = 0
 let losses = 0
 let target = Math.floor(Math.random() * 100 + 40)
 let score = 0
-let crystalA = Math.floor(Math.random() * 12)
-let crystalB = Math.floor(Math.random() * 12)
-let crystalC = Math.floor(Math.random() * 12)
-let crystalD = Math.floor(Math.random() * 12)
+let crystalA = Math.floor(Math.random() * 11 + 1)
+let crystalB = Math.floor(Math.random() * 11 + 1)
+let crystalC = Math.floor(Math.random() * 11 + 1)
+let crystalD = Math.floor(Math.random() * 11 + 1)
 
 //New Game
 const restart = function(){
@@ -17,21 +17,21 @@ const restart = function(){
   document.getElementById("wins").textContent=`Wins: ${wins}`
   document.getElementById("losses").textContent=`Losses: ${losses}`
   document.getElementById("score").textContent=`Your total score is: ${score}`
-  crystalA = Math.floor(Math.random() * 12)
-  crystalB = Math.floor(Math.random() * 12)
-  crystalC = Math.floor(Math.random() * 12)
-  crystalD = Math.floor(Math.random() * 12)
+  crystalA = Math.floor(Math.random() * 11 + 1)
+  crystalB = Math.floor(Math.random() * 11 + 1)
+  crystalC = Math.floor(Math.random() * 11 + 1)
+  crystalD = Math.floor(Math.random() * 11 + 1)
 }
 restart()
   document.getElementById("crystalA").addEventListener("click", function(){
     score = score + crystalA
     document.getElementById("score").textContent=`Your total score is: ${score}`
     if (score === target) {
-      alert("You win!")
+      alert("Great job! Let's go again!")
       wins = wins + 1
       restart()
     } else if (score > target) {
-      alert("You lose!")
+      alert("Oops! Looks like your score went too high!")
       losses = losses + 1
       restart ()
     }
@@ -40,11 +40,11 @@ restart()
     score = score + crystalB
     document.getElementById("score").textContent=`Your total score is: ${score}`
     if (score === target) {
-      alert("You win!")
+      alert("Great job! Let's go again!")
       wins = wins + 1
       restart()
     } else if (score > target) {
-      alert("You lose!")
+      alert("Oops! Looks like your score went too high!")
       losses = losses + 1
       restart ()
     }
@@ -53,11 +53,11 @@ restart()
     score = score + crystalC
     document.getElementById("score").textContent=`Your total score is: ${score}`
     if (score === target) {
-      alert("You win!")
+      alert("Great job! Let's go again!")
       wins = wins + 1
       restart()
     } else if (score > target) {
-      alert("You lose!")
+      alert("Oops! Looks like your score went too high!")
       losses = losses + 1
       restart ()
     }
@@ -66,11 +66,11 @@ restart()
     score = score + crystalD
     document.getElementById("score").textContent=`Your total score is: ${score}`
     if (score === target) {
-      alert("You win!")
+      alert("Great job! Let's go again!")
       wins = wins + 1
       restart()
     } else if (score > target) {
-      alert("You lose!")
+      alert("Oops! Looks like your score went too high!")
       losses = losses + 1
       restart ()
     }
